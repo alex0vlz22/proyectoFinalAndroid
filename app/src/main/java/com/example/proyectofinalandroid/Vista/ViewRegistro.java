e023e8f454f5b165e7875f75e6afa1ce3e4b28d2
Junior Alexis Llantén Vélez <junioralexislv@gmail.com>
2020-10-22 15:28:02 -0500

    Commit de prueba

--- app/src/main/java/com/example/proyectofinalandroid/Vista/ViewRegistro.java
+++ app/src/main/java/com/example/proyectofinalandroid/Vista/ViewRegistro.java
@@ -1,14 +1,14 @@
-package com.example.proyectofinalandroid;
+package com.example.proyectofinalandroid.Vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ViewRegistro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_registro);
     }
 }
